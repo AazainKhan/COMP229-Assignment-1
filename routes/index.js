088d19b22b645182048c716cd4e3236a496a70a7
Aazain <43759637+Aazainkhan@users.noreply.github.com>
2023-10-28 00:21:04 -0400

    Project section

--- routes/index.js
+++ routes/index.js
@@ -8,22 +8,22 @@ router.get('/', function(req, res, next) {
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About page' });
+  res.render('about', { title: 'About' });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact page' });
+  res.render('contact', { title: 'Contact' });
 });
 
 /* GET projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects page' });
+  res.render('projects', { title: 'Projects' });
 });
 
 /* GET services page. */
 router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'Services page' });
+  res.render('services', { title: 'Services' });
 });
 
 module.exports = router;
